c965fa22afadec68ed3bb0c129849fdad990c41b
DaBlower <113282214+DaBlower@users.noreply.github.com>
2023-08-01 19:49:38 +1000

    builds

--- Main/Assets/Scripts/CameraController.cs
+++ Main/Assets/Scripts/CameraController.cs
@@ -2,14 +2,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //Room camera (Delete for production)
     [SerializeField] private float speed;
     private float currentPosX;
     private Vector3 velocity = Vector3.zero;
 
+    //Follow camera
+    [SerializeField] Transform player;
+    [SerializeField] private float cameraDistance;
+    [SerializeField] private float cameraSpeed;
+    private float lookAhead;
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        //Room camera (Delete for production)
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
 
+        //Follow camer
+        transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
+        lookAhead = Mathf.Lerp(lookAhead, (cameraDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
     }
 
     public void MoveToNewRoom(Transform _newRoom)
