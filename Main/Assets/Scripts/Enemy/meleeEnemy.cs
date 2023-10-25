dac7262fc0a3b91c2c34d81620e24e68195d95f6
DaBlower <113282214+DaBlower@users.noreply.github.com>
2023-10-25 20:39:10 +1100

    10th pt2

--- Main/Assets/Scripts/Enemy/meleeEnemy.cs
+++ Main/Assets/Scripts/Enemy/meleeEnemy.cs
@@ -9,6 +9,14 @@ public class meleeEnemy : MonoBehaviour
     [SerializeField] private BoxCollider2D boxCollider;
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
+    //References
+    private Animator anim;
+    private Health playerHealth;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
@@ -18,6 +26,8 @@ public class meleeEnemy : MonoBehaviour
             if (cooldownTimer >= attackCooldown)
             {
                 //Attack
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
             }
         }
     }
@@ -30,6 +40,9 @@ public class meleeEnemy : MonoBehaviour
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
             
+        if(hit.collider!= null)
+            playerHealth = hit.transform.GetComponent<Health>();
+
         return hit.collider != null;
     }
 
@@ -37,6 +50,12 @@ public class meleeEnemy : MonoBehaviour
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void damagePlayer()
+    {
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
     }
 }
 
