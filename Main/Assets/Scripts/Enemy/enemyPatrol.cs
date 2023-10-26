5cd999efa41b34bfe0684fb4cf8287a42afb8816
DaBlower <113282214+DaBlower@users.noreply.github.com>
2023-10-26 19:03:06 +1100

    Finish basic pathfinding and attacking

--- Main/Assets/Scripts/Enemy/enemyPatrol.cs
+++ Main/Assets/Scripts/Enemy/enemyPatrol.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
 
-public class enemyPatrol : MonoBehaviour
+public class EnemyPatrol : MonoBehaviour
 {
-    [Header ("Patrol Points")]
+    [Header("Patrol Points")]
     [SerializeField] private Transform leftEdge;
     [SerializeField] private Transform rightEdge;
 
-    [Header ("Enemy")]
+    [Header("Enemy")]
     [SerializeField] private Transform enemy;
-    
-    [Header ("Movement parameters")]
+
+    [Header("Movement parameters")]
     [SerializeField] private float speed;
     private Vector3 initScale;
     private bool movingLeft;
+
+    [Header("Idle Behaviour")]
+    [SerializeField] private float idleDuration;
+    private float idleTimer;
 
     [Header("Enemy Animator")]
     [SerializeField] private Animator anim;
@@ -21,43 +25,48 @@ public class enemyPatrol : MonoBehaviour
     {
         initScale = enemy.localScale;
     }
+    private void OnDisable()
+    {
+        anim.SetBool("moving", false);
+    }
 
     private void Update()
     {
-        if(movingLeft)
+        if (movingLeft)
         {
-            if(enemy.position.x >= leftEdge.position.x)
+            if (enemy.position.x >= leftEdge.position.x)
                 MoveInDirection(-1);
             else
-            {
                 DirectionChange();
-            }
         }
         else
         {
-            if(enemy.position.x <= rightEdge.position.x)
+            if (enemy.position.x <= rightEdge.position.x)
                 MoveInDirection(1);
             else
-            {
                 DirectionChange();
-            }
         }
-
-
     }
 
     private void DirectionChange()
     {
         anim.SetBool("moving", false);
-        movingLeft = !movingLeft;
+        idleTimer += Time.deltaTime;
+
+        if (idleTimer > idleDuration)
+            movingLeft = !movingLeft;
     }
+
     private void MoveInDirection(int _direction)
     {
+        idleTimer = 0;
         anim.SetBool("moving", true);
 
+        //Make enemy face direction
         enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction,
             initScale.y, initScale.z);
 
+        //Move in that direction
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
             enemy.position.y, enemy.position.z);
     }
--- Main/Assets/Scripts/Enemy/meleeEnemy.cs
+++ Main/Assets/Scripts/Enemy/meleeEnemy.cs
@@ -1,37 +1,48 @@
 using UnityEngine;
 
-public class meleeEnemy : MonoBehaviour
+public class MeleeEnemy : MonoBehaviour
 {
+    [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
-    [SerializeField] private float colliderDistance;
     [SerializeField] private int damage;
+
+    [Header("Collider Parameters")]
+    [SerializeField] private float colliderDistance;
     [SerializeField] private BoxCollider2D boxCollider;
+
+    [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
+
     //References
     private Animator anim;
     private Health playerHealth;
+    private EnemyPatrol enemyPatrol;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
     private void Update()
     {
         cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
         if (PlayerInSight())
         {
             if (cooldownTimer >= attackCooldown)
             {
-                //Attack
                 cooldownTimer = 0;
                 anim.SetTrigger("meleeAttack");
             }
         }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
     }
-        
 
     private bool PlayerInSight()
     {
@@ -39,24 +50,22 @@ public class meleeEnemy : MonoBehaviour
             Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
-            
-        if(hit.collider!= null)
+
+        if (hit.collider != null)
             playerHealth = hit.transform.GetComponent<Health>();
 
         return hit.collider != null;
     }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 
-    private void damagePlayer()
+    private void DamagePlayer()
     {
         if (PlayerInSight())
             playerHealth.TakeDamage(damage);
     }
 }
-
-//17:15
